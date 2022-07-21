57e535af8a78ad4b58396ccbb45f82b4220fc380
VinithaDar <vinithachowdary.d@gmail.com>
2022-07-20 23:18:01 -0400

    Adding Wait Concepts

---
+++ SeleniumPactice/April2022SeleniumSession/src/main/java/seleniumsessions/WaitForAlert.java
@@ -0,0 +1,52 @@
+package seleniumsessions;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WaitForAlert {
+       static WebDriver driver;
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+        
+		driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
+		driver.findElement(By.name("proceed")).click();
+		
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+		
+		String text = alert.getText();
+		System.out.println(text);
+		
+		alert.sendKeys("testing");
+		
+		alert.accept();//accept the alert
+		//alert.sendKeys("testing");
+		//alert.dismiss(); //dismiss the alert
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
