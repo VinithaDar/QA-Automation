3e20f87b5128093f5ec3b1e869b336c69070294b
VinithaDar <100165617+VinithaDar@users.noreply.github.com>
2022-07-21 00:05:38 -0400

    Adding customized_wait (#1)
    * Adding customized_wait
    
    * Addressing pull request comments

---
+++ SeleniumPactice/April2022SeleniumSession/src/main/java/seleniumsessions/CustomizedWait.java
@@ -0,0 +1,18 @@
+package seleniumsessions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CustomizedWait {
+         static WebDriver driver;
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		
+		driver.get("https://www.amazon.com/");
+		//waitForPageLoad(10);
+     }
+}
